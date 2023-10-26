f58c408298f457460f4edeb81ca0fd93a219275b
Jasminepressley <Jasminerpressley@gmail.com>
2023-10-26 15:59:09 -0700

    update

---
+++ functions.sh
@@ -0,0 +1,24 @@
+ #!/bin/bash
+
+# Script Name:                        Functions.sh
+# Author:                             Jasmine Pressley
+# Date of latest revision:            10/26/2023
+# Purpose:                            Ops Challenge Class 03
+
+# Declaration of variables
+
+
+
+# Declaration of functions
+
+print_something() {
+    last
+    echo $login
+}
+
+
+# Main
+print_something
+print_something
+print_something
+
