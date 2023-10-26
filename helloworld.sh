5ef4ea8a9084b5bbda4c6123ee0df7fcca95c5ee
Jasmineoressley <Jasminerpressley@gmail.com>
2023-10-26 13:53:23 -0700

    updated

---
+++ helloworld.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script Name:                  helloworld.sh
+# Author:                       Jasmine Pressley
+# Date of latest revision:      10/25/2023
+# Purpose:                      Ops Challenge 201
+
+# Declaration of variables
+my_variable="Hello world!"
+
+# Declaration of functions
+
+
+# Main
+echo $my_variable
+
+
+
+# End
